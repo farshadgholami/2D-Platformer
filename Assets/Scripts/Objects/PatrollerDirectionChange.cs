e77138df7fcf82eb7595af4bf51818403cfee67d
yfs <Yashar.fallah1372@gmail.com>
2019-07-12 13:01:30 +0430

    Fixed a bug where loading scenes will cause patrollers to lost direction

--- Assets/Scripts/Objects/PatrollerDirectionChange.cs
+++ Assets/Scripts/Objects/PatrollerDirectionChange.cs
@@ -6,11 +6,26 @@ public class PatrollerDirectionChange : MonoBehaviour
 {
     [SerializeField]
     private Side side;
+
+    private Collider2D collider_;
+
+    private void Start()
+    {
+        collider_ = GetComponent<Collider2D>();
+        GameManager.LoadSceneAction += Reload;   
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Patroller"))
         {
             collision.GetComponent<PatrollerBrain>().SetTarget(transform.position, Toolkit.SideToVector(side));
         }
+    }
+
+    private void Reload()
+    {
+        collider_.enabled = false;
+        collider_.enabled = true;
     }
 }
