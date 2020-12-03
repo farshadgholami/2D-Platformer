5dc954350843517715c71e3b585ac5d0d872854e
Farshad Gholami <farshad.gholami@outlook.com>
2020-12-03 22:05:49 +0330

    Add jump pad feature

---
+++ Assets/Scripts/ImpactEffects/JumpPad.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpPad : ImpactEffect
+{
+    [SerializeField] private List<string> effectedTags;
+    protected int lastJump;
+    
+    protected override void Effect(GameObject impacted)
+    {
+        if(!effectedTags.Contains(impacted.tag)) return;
+        var jump = impacted.GetComponent<CharacterJump>();
+        var stats = impacted.GetComponent<CharacterStats>();
+        if (lastJump != Time.frameCount)
+        {
+            lastJump = Time.frameCount;
+            stats.FeetState = FeetStateE.OnGround;
+            jump.HitJump();
+        }
+    }
+    protected override void ImpactCheck()
+    {
+        foreach (ImpactProperty impactproperty in physic.ImpactProperties)
+        {
+            if (!effectedTags.Contains(impactproperty.impactEffect.tag)) continue;
+            if (effectiveSides.Contains(Toolkit.VectorToSide(impactproperty.impactSide))) return;
+            Effect(impactproperty.impactEffect.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Physics/NormalPhysic.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class NormalPhysic : Physic
+{
+    [SerializeField] private LayerMask layer;
+    protected override void Init()
+    {
+        self_layer_mask_ = gameObject.layer;
+        collider2d = GetComponent<BoxCollider2D>();
+        size = transform.localScale * collider2d.size;
+        layerMask += layer.value;
+        CalculateRayCastPoints();
+        impactProperties = new List<ImpactProperty>();
+        impacted_objects_ = new List<GameObject>();
+        v_raycast_list_ = new List<RaycastHit2D>();
+        h_raycast_list_ = new List<RaycastHit2D>();
+    }
+}
